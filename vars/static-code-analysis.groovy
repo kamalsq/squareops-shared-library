d68d0838fdba3e534a8d4361204b9b92fedd9f02
Kamal <93322430+kamalsq@users.noreply.github.com>
2023-03-23 01:51:31 +0530

    Update static-code-analysis.groovy

--- vars/static-code-analysis.groovy
+++ vars/static-code-analysis.groovy
@@ -1,5 +1,22 @@
 def call(){
             echo "NodejsScan"
             nodejsscan --directory `pwd`
-            '''
-}
+          
+//          echo 'SonarQube Analysis'
+//          echo '${scannerHome}'
+//          '${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${DOCKER_REPO_NAME}-${DEPLOYMENT_STAGE} -Dsonar.qualitygate=my_custom_quality_gate''
+//         }
+// Sonarqube Quality Gate
+//         echo 'SonarQube Quality gate'
+//         timeout(time: 1, unit: 'HOURS') {
+//         waitForQualityGate abortPipeline: true
+//    }
+// git-secret scan
+//             echo 'git secret scanning'
+//             git config --global --add safe.directory /home/jenkins/agent/workspace/${DOCKER_REPO_NAME}
+//             git secrets --register-aws
+//             git-secrets --scan
+//             
+//         }
+//     }
+     }
