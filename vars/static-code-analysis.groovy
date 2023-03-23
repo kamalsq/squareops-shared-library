e03bb7f1556e9cf438505bc8855db4ad19959d0d
Kamal <93322430+kamalsq@users.noreply.github.com>
2023-03-23 07:04:10 +0530

    Update static-code-analysis.groovy

--- vars/static-code-analysis.groovy
+++ vars/static-code-analysis.groovy
@@ -1,16 +1,17 @@
 def call(){
             sh ''' echo "NodejsScan" nodejsscan --directory `pwd` '''
           
-//          echo 'SonarQube Analysis'
-//          echo '${scannerHome}'
-//          '${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${DOCKER_REPO_NAME}-${DEPLOYMENT_STAGE} -Dsonar.qualitygate=my_custom_quality_gate''
-//         }
 // Sonarqube Analysis
-//        withSonarQubeEnv ('SonarqubeScanner') {
-//            sh 'echo SonarQube Analysis'
-//            sh 'echo ${scannerHome}'
-//          sh '${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${DOCKER_REPO_NAME}-${DEPLOYMENT_STAGE} -Dsonar.qualitygate=my_custom_quality_gate''
-//         }
+        withSonarQubeEnv ('SonarqubeScanner') {
+            sh 'echo SonarQube Analysis'
+            sh 'echo ${scannerHome}'
+		sh '${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${DOCKER_REPO_NAME}-${DEPLOYMENT_STAGE} -Dsonar.qualitygate=my_custom_quality_gate''
+         }
+// Sonarqube Quality Gate
+         sh 'echo SonarQube Quality gate'
+         timeout(time: 1, unit: 'HOURS') {
+         waitForQualityGate abortPipeline: true
+       }
 // git-secret scan
 //             echo 'git secret scanning'
 //             git config --global --add safe.directory /home/jenkins/agent/workspace/${DOCKER_REPO_NAME}
